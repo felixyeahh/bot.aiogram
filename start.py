7668f20b123e5c139d3f7edef8ec8ced5b6f5942
felix <felixyeah@outlook.com>
2022-12-17 10:54:44 -0800

    adding warns system

--- start.py
+++ start.py
@@ -3,11 +3,35 @@ from aiogram.utils import executor
 from bot import dp
 from handlers import client, admin, other
 from database import sqlite_db
+from filters.admin import IsAdminFilter
+from servises.set_comm import set_default_commands
+from database.sqlite_db import get_engine, create_database
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
+logger = logging.getLogger(__name__)
+
+def register_all_middlewares(dispatcher: Dispatcher):
+    logger.info('Registering middlewares')
 
 
+def register_all_filters(dispatcher: Dispatcher) -> None:
+    logger.info('Registering filters')
+    dispatcher.filters_factory.bind(IsAdminFilter)
 
+
+async def set_all_default_commands(bot: Bot):
+    await set_default_commands(bot)
+    logger.info('Registering commands')
+
+
+def create_db(bot: Bot):
+    sqlite_file_name = 'ench.db'
+    sqlite_url = f'sqlite:///{sqlite_file_name}'
+    engine = get_engine(sqlite_url)
+    create_database(engine)
+    bot['engine'] = engine
+    logger.info('Database was created')
+    create_db(dispatcher.bot)
 
 #-------------------------------------------------startup-----------------------------
 async def on_startup(_):
@@ -21,10 +45,3 @@ other.register_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup) #start
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-''' #TODO 
-1.
-2.
-3.
-'''
