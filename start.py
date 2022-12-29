223568e266bc201e71fbe1b40679dc6da5b96e2a
felix <felixyeah@outlook.com>
2022-12-29 08:20:22 -0800

    finally, updated

--- start.py
+++ start.py
@@ -1,30 +1,31 @@
-#------------------------------------------------------imports----------------------------------------------------
-from aiogram.utils import executor
-from bot import dp
-from handlers import client, admin, other
+
+# ? ---------imports
+from bot import bot, dp, Bot
+from aiogram import Dispatcher, utils
+from handlers import client, admin, other, roleplay
 from database import sqlite_db
-#--------------------------------------------------------------------------------------------------------------------------------------------------------
+from servises.set_comm import set_default_commands
+from database.sqlite_db import get_engine, create_database
 
+async def set_all_default_commands(bot: Bot): # ? setting-commands
+    await set_default_commands(bot)
+async def create_db(bot: Bot): # ? creating-database
+    sqlite_file_name = 'ench.db'
+    sqlite_url = f'sqlite:///{sqlite_file_name}'
+    engine = get_engine(sqlite_url)
+    create_database(engine)
+    bot['engine'] = engine
 
-
-
-
-#-------------------------------------------------startup-----------------------------
-async def on_startup(_):
-    print('\n\n\n\n\n^bot connected\n\n\n\n\n')
-    sqlite_db.sql_start() #db connect
-
-#handlers-connect
+async def on_startup(dispatcher: Dispatcher): # ? on-startup
+    sqlite_db.sql_start()
+    await create_db(dispatcher.bot)
+    await set_all_default_commands(dispatcher.bot)
+    await bot.send_message(-1001847368960, '>>> bot connected')
+    
+# ? -----------registering-handlers
 client.register_client(dp)
 admin.register_admin(dp)
 other.register_other(dp)
+roleplay.register_roleplay(dp)
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup) #start
-#--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-''' #TODO 
-1.
-2.
-3.
-'''
+utils.executor.start_polling(dp, skip_updates=True, on_startup=on_startup) # ! starting the bot
